0efe21de5e7b0b1288b97e577baa34dbab37ee6d
zavanton123 <zavanton123@gmail.com>
2021-06-02 19:24:06 +0300

    post added

--- api/src/controllers/api-controller.js
+++ api/src/controllers/api-controller.js
@@ -7,9 +7,7 @@ exports.getStatus = (req, res, next) => {
 };
 
 exports.createPost = async (req, res, next) => {
-  let title = req.body.title;
-  console.log(`zavanton - creating a new post: ${title}`);
-  const post = await Post.create({title: "hello mongo"});
+  const post = await Post.create({title: req.body.title});
   return res.json({
     message: 'Post created',
     post: post
