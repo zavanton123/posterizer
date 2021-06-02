b0608d3178992e451e6d0008468420a3b7592134
zavanton123 <zavanton123@gmail.com>
2021-06-02 15:53:57 +0300

    env variables are used

---
+++ README.md
@@ -0,0 +1,13 @@
+# Posterizer Project
+
+### How to run a debug build?
+Create a file /api/.env
+> DBHOST=localhost
+>
+> DBPORT=27017
+>
+> DBNAME=posterizer
+>
+> DBUSER=some-user-name
+>
+> DBPASSWORD=some-password
--- api/src/database/connection.js
+++ api/src/database/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const {
+  DBHOST, DBPORT, DBNAME, DBUSER, DBPASSWORD
+} = process.env;
 
-const url = 'mongodb://zavanton:21665mylife@localhost:27017/posterizer?authSource=admin';
+const url = `mongodb://${DBUSER}:${DBPASSWORD}@${DBHOST}:${DBPORT}/${DBNAME}?authSource=admin`;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true
