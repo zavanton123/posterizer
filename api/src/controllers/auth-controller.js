8ba45e21a864eb042d7a6cb2bbc831b23fe35afa
zavanton123 <zavanton123@gmail.com>
2021-06-04 17:20:08 +0300

    simple password validation added

--- api/src/controllers/auth-controller.js
+++ api/src/controllers/auth-controller.js
@@ -14,22 +14,32 @@ exports.signup = async (req, res, next) => {
   const password = req.body.password;
   const confirmPassword = req.body['confirm-password'];
 
-  // Ensure no user with the same username exists
+  await ensureUniqueUsername(username, next);
+  await ensureUniqueEmail(email, next, res);
+
+  if (password !== confirmPassword) {
+    const error = new Error(`The confirm password must be the same as the password!`);
+    error.statusCode = HTTP_CONFLICT_ERROR;
+    next(error);
+  } else {
+    return res.json({message: 'Ok'});
+  }
+}
+
+async function ensureUniqueUsername(username, next) {
   const dbUser = await User.find({username: username});
   if (dbUser.length > 0) {
     const error = new Error(`A user with the username ${username} already exists!`);
     error.statusCode = HTTP_CONFLICT_ERROR;
     next(error);
   }
+}
 
-  // Ensure no user with the same email exists
+async function ensureUniqueEmail(email, next, res) {
   const emailUser = await User.find({email: email});
   if (emailUser.length > 0) {
     const error = new Error(`A user with the email ${email} already exists!`);
     error.statusCode = HTTP_CONFLICT_ERROR;
     next(error);
-  } else {
-    return res.json({message: `Signup`});
   }
-
 }
