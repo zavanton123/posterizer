ab1559baf513fd8d3d07376f9143719cc1b4fa44
zavanton123 <zavanton123@gmail.com>
2021-06-04 18:42:00 +0300

    user login added

--- api/src/controllers/auth-controller.js
+++ api/src/controllers/auth-controller.js
@@ -1,10 +1,55 @@
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const {processError} = require("../utils/errors");
-const {HTTP_CREATED, HTTP_CONFLICT_ERROR} = require("../utils/constants");
+const {HTTP_CREATED, HTTP_CONFLICT_ERROR, HTTP_NOT_AUTHENTICATED} = require("../utils/constants");
 const {User} = require('../models/models');
+const {APP_SECRET} = process.env;
 
-exports.login = (req, res, next) => {
-  return res.json({message: `Login`});
+const checkUserExists = async (username) => {
+  const user = await User.findOne({username: username});
+  if (!user) {
+    const error = new Error('The user with this username does not exist');
+    error.statusCode = HTTP_NOT_AUTHENTICATED;
+    throw error;
+  }
+  return user;
+}
+
+const checkPassword = async (password, user) => {
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    const error = new Error('Wrong password!');
+    error.statusCode = HTTP_NOT_AUTHENTICATED;
+    throw error;
+  }
+}
+
+function createToken(user) {
+  return jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+      email: user.email
+    },
+    APP_SECRET,
+    {expiresIn: '1d'}
+  );
+}
+
+exports.login = async (req, res, next) => {
+  try {
+    const user = await checkUserExists(req.body.username);
+    await checkPassword(req.body.password, user)
+    const token = createToken(user);
+
+    return res.json({
+      message: "Login success",
+      id: user._id,
+      token: token
+    });
+  } catch (err) {
+    processError(err, next);
+  }
 }
 
 exports.signup = async (req, res, next) => {
--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -2,5 +2,6 @@ exports.APP_PORT = 3000;
 
 exports.HTTP_CREATED = 201;
 exports.HTTP_OK = 200;
-exports.HTTP_SERVER_ERROR = 500;
+exports.HTTP_NOT_AUTHENTICATED = 401;
 exports.HTTP_CONFLICT_ERROR = 409;
+exports.HTTP_SERVER_ERROR = 500;
